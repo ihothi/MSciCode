e79ded4c5ad738e6608918faf1af3c9da5ee7a8f
Ian <zcapiho@ucl.ac.uk>
2017-11-11 11:14:56 +0000

    Creating new python script which contains functions and made a new notebook to run these

--- ProjectF.py
+++ ProjectF.py
@@ -1,27 +1,112 @@
 import numpy as np
-from astropy.io import fits
 
-def Reading(list):
-    """ Input: list - name of the .txt file that contains list of .fits files 
-        Output: F - a list containing names of .fits files"""
-    F = open(list, 'w') 
-    return F
-
-
-class Object:
+def MLAData(Full_Data,BinInfos,Flux):
     
-    def __init__():[]
-   
-    def __init__(self, SDSS_NAME, R, D, Z_VI, CLASS_P, p, mjd, fid):
-        #leaving out redshift for now
-        R = round(R,2)
-        D =  round(D,2)
-        self.name = SDSS_NAME
-        self.RA = R
-        self.Dec = D
-        self.z = Z_VI
-        self.Class_p = CLASS_P
-        self.Plate = p
-        self.MJD = mjd
-        self.FiberID = fid
+    Y = []
+    X = []
+    All_redshifts=[]
+    All_Mag=[]
+    plate_no = 0
+    y=0
+    while plate_no < len(Full_Data):
+        #loading matched objects with sup, plate data
+        CurrentSup_data = Full_Data[plate_no]
+        CurrentBin = BinInfos[plate_no]
+        CurrentFlux = Flux[plate_no]
+        #first object is zeroth element
+        Sup_obj =0 
+        while Sup_obj < len(CurrentSup_data):
+            #to stop double counting
+            no_match = True
+            #looking at an object that has been matched in sup list
+            CurrentSup = CurrentSup_data[Sup_obj]            
+            #going through each object in the bin
+            BinObj_No = 0
+            while BinObj_No<len(CurrentBin):
+                BinObj = CurrentBin[BinObj_No]
+                ##checking if the two match 
+                if BinObj['FIBERID'] == CurrentSup.FiberID:
+                    y=y+1
+                    if no_match:
+                        if CurrentSup.Class_p == 0:
+                            a=0
+                        else:
+    
+                            no_match = False
+                            Y.append(CurrentSup.Class_p)
+                            x_flux =(CurrentFlux[BinObj_No])
+                            x_flux=x_flux[:4600]
+                            X.append(x_flux)
+                            All_redshifts.append(CurrentSup_test.z)
+                            All_Mag.append(CurrentSup_test.Mag)
+                BinObj_No=BinObj_No+1  
+            Sup_obj=Sup_obj+1
+        plate_no = plate_no+1
+
+    return X,Y,All_redshifts,All_Mag
+
+
+def classification(objectclass, Trainingclass, prediction):
+    ## Star, Quasar, Galaxy, BAL
+    classi = []
+    star=0;
+    qso=0;
+    gal=0;
+    bal=0;
+    i=0
+    while i< len(Trainingclass):
+        currentobject = Trainingclass[i]
+        if objectclass==1:
+            if currentobject==1: 
+                star=star+1
+            elif currentobject==3: 
+                qso=qso+1
+            elif currentobject==4: 
+                gal=gal+1
+            elif currentobject==30: 
+                bal=bal+1
+                
+
+        elif objectclass==3:
+            if currentobject==1: 
+                star=star+1
+            elif currentobject==3: 
+                qso=qso+1
+            elif currentobject==4: 
+                gal=gal+1
+            elif currentobject==30: 
+                bal=bal+1
+                
+        elif objectclass==4:
+            if currentobject==1: 
+                star=star+1
+            elif currentobject==3: 
+                qso=qso+1
+            elif currentobject==4: 
+                gal=gal+1
+            elif currentobject==30: 
+                bal=bal+1
+                
+
+        elif objectclass==30:
+            if currentobject==1: 
+                star=star+1
+            elif currentobject==3: 
+                qso=qso+1
+            elif currentobject==4: 
+                gal=gal+1
+            elif currentobject==30: 
+                bal=bal+1
         
+        i=i+1
+    
+    classi.append(star)
+    classi.append(qso)
+    classi.append(gal)
+    classi.append(bal)
+    return classi
+        
+                
+    
+    
+
