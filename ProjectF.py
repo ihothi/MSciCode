941464d5d751ec26e74b24242d332502e1575fef
Ian <zcapiho@ucl.ac.uk>
2017-12-09 16:53:20 +0000

    Rebin plots

--- ProjectF.py
+++ ProjectF.py
@@ -32,6 +32,7 @@ class FluxStore:
         
         
         
+        
 def Rebin(Plate_hdu, Bin_Size ):
     
     rebin = []
@@ -75,7 +76,161 @@ def Rebin(Plate_hdu, Bin_Size ):
     return rebin, rebin_weight
 
 
+def StandardRebin(Plate_hdu,wavelength, Bin_Size ):
+    
+    rebin = []
+    rebin_weight=[]
+    bin_wav = Bin_Size*0.829026074968624
+    wavelength=wavelength[:4600]
+    rebinwav=[]
+ 
+    obj=0
+    while obj < len(Plate_hdu):
+        last_wav=0
+        current_flux = Plate_hdu[obj][0]
+        current_mask = Plate_hdu[obj][2]
+        current_inv = Plate_hdu[obj][3]
+        rebin_flux=[]
+        weight_=[]
+        cwav=[]
+        pix=0
+        First = True
+        bin_max = 3600
+        while pix < len(current_flux):
+            bin_no=0
+            x_flux=0
+            W=0
+            wav=0
+            if First: 
+                cwav.append(bin_max+(bin_wav*0.5))
+                bin_max = bin_max+bin_wav
+                
+                First = False
+            else: 
+                bin_max = bin_max+bin_wav
+                cwav.append(bin_max+(bin_wav*0.5))
+            while wav<bin_max:
+                
+                if pix< len(current_flux):
+                    w_ = 0
+                    m = current_mask[pix]
+                    wav = wavelength[pix]
+                    if m==0:
+                        w_=0
+                    else:
+                        w_ = current_inv[pix]
+                    x_flux = x_flux +(current_flux[pix]*w_)
+                    W=W+w_
+                    pix=pix+1
+                else: 
+                    pix=pix+1
+                    wav=bin_max
+                    
+    
+            if W == 0:
+                x_flux=0
+            else: 
+                x_flux = x_flux/W
+            rebin_flux.append(x_flux)
 
+            weight_.append(W)
+        rebinwav.append(cwav)
+        rebin.append(rebin_flux)
+        rebin_weight.append(weight_)
+        obj=obj+1
+    return rebin, rebin_weight,rebinwav
+
+
+
+def MLADatabasic(Full_Data,BinInfos,Flux,log_wavs):
+    
+    
+    All_Y=[]
+    All_X = []
+    All_redshifts=[]
+    All_Mag=[]
+    All_Name=[]
+    wav_logs=[]
+    plate_no = 0
+    y=0
+    while plate_no < len(Full_Data):
+        #loading matched objects with sup, plate data
+        CurrentSup_data = Full_Data[plate_no]
+        CurrentBin = BinInfos[plate_no]
+        CurrentFlux = Flux[plate_no]
+        wav= log_wavs[plate_no]
+        Plate_Y = []
+        Plate_X = []
+        Plate_redshifts=[]
+        Plate_Mag=[]
+        Plate_Name=[]
+        
+        #first object is zeroth element
+        Sup_obj =0 
+        while Sup_obj < len(CurrentSup_data):
+            #to stop double counting
+            no_match = True
+            #looking at an object that has been matched in sup list
+            CurrentSup = CurrentSup_data[Sup_obj]            
+            #going through each object in the bin
+            BinObj_No = 0
+            while BinObj_No<len(CurrentBin):
+                BinObj = CurrentBin[BinObj_No]
+                
+                
+                ##checking if the two match 
+                if BinObj['FIBERID'] == CurrentSup.FiberID:
+                    y=y+1 
+                    if no_match:
+                        if CurrentSup.Class_p == 0:
+                            a=0
+                        else:
+    
+                            no_match = False
+                            if CurrentSup.Class_p == 3 or CurrentSup.Class_p==30:
+                                if CurrentSup.z < 2.1:
+                                    Plate_Y.append(3)
+                                    x_flux =(CurrentFlux[BinObj_No])
+                                    x_flux=x_flux[:4600]
+                                    Plate_X.append(x_flux)
+                                    Plate_redshifts.append(CurrentSup.z)
+                                    Plate_Mag.append(CurrentSup.Mag)
+                                    Plate_Name.append(CurrentSup.name)
+                                    
+                                else:
+                                    Plate_Y.append(30)
+                                    x_flux =(CurrentFlux[BinObj_No])
+                                    x_flux=x_flux[:4600]
+                                    Plate_X.append(x_flux)
+                                    Plate_redshifts.append(CurrentSup.z)
+                                   
+                                    Plate_Mag.append(CurrentSup.Mag)
+                                    Plate_Name.append(CurrentSup.name)
+                        
+                                    
+                            else: 
+                                Plate_Y.append(CurrentSup.Class_p)
+                                x_flux =(CurrentFlux[BinObj_No])
+                                x_flux=x_flux[:4600]
+                                Plate_X.append(x_flux)
+                                Plate_redshifts.append(CurrentSup.z)
+                               
+                                Plate_Mag.append(CurrentSup.Mag)
+                                Plate_Name.append(CurrentSup.name)
+            
+                BinObj_No=BinObj_No+1  
+            Sup_obj=Sup_obj+1
+        All_Y.append(Plate_Y)
+        All_X.append(Plate_X)
+        All_Name.append(Plate_Name)
+        plate_no = plate_no+1
+        wav_logs.append(wav)
+        All_redshifts.append(Plate_redshifts)
+        All_Mag.append(Plate_Mag)
+      
+    
+
+    return All_X,All_Y,All_redshifts,All_Mag,wav_logs,All_Name
 
 
 
@@ -229,21 +384,26 @@ def classification(objectclass, Trainingclass, prediction):
     return classi,starloc,qsoloc,galloc,balloc
 
 
-def storing(PLATEIDs,supers):
+def storing(PLATEIDs,s):
     CurrentPlate = 0
     Full_Data=[]
     Plate_Count =0
+    supers = s
     while Plate_Count < len(PLATEIDs):
         Current_Plate = PLATEIDs[Plate_Count]
         CurrentObject = 0
         platename_data = []
-        while CurrentObject < len(supers):
+        currentSup_size =len(supers)
+        while CurrentObject < currentSup_size:
             Current = supers[CurrentObject]
             if Current['PLATE'] == Current_Plate:
+                 
                 Object_ = Object(Current['SDSS_NAME'], Current['RA'], Current['Dec'], 
                          Current['Z_VI'], Current['CLASS_PERSON'],Current['PLATE'] ,
                          Current['MJD'], Current['FIBERID'],Current['PSFMAG'])
                 platename_data.append(Object_)
+                ## Once matched, no point
+                del supers[CurrentObject] 
             CurrentObject=CurrentObject+1    
         Plate_Count = Plate_Count + 1
         Full_Data.append(platename_data)
